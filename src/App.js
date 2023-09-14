22115b24467087942f3ca1bbc09be6a6e615a94e
Mrzakirullahkhan <mrzak802@gmail.com>
2023-09-15 01:55:35 +0500

    selection-app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,70 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from "react";
 import './App.css';
 
-function App() {
+function SelectionApp() {
+  const emoji = "\u{1F604}"
+  const [number, setnumber] = useState(1);
+  const [message, setmessage] = useState([
+    `Believe in yourself, have confidence and feel good ${"\u{1F44D}"}`,
+     `Expect the best possible outcome for what you do.${'\u{1F60D}'}`,
+    `  Set goals so that you can reach them ${"\u{1F604}"}`,
+    `Never give up. Keep going and keep trying.${'\u{270C}\u{1F3FB}'}`,
+    "Take care of yourself through a healthy diet, exercise"
+  ])
+  // function for nect btn
+  function nextBtnHandler() {
+    if (number < 5) {
+      setnumber(number + 1)
+
+    }
+  }
+  // function for previous btn 
+  function previousBtnHandler() {
+    if (number > 0) {
+      setnumber(number - 1)
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+    <>
+
+      <div className="parentDiv">
+
+        <div className="mainDiv">
+
+
+          {
+            Array.from([1, 2, 3,4,5], x => (
+              <div className="numDiv" style={{ fontSize: "30px", borderRadius: "50%", backgroundColor: number >= x ? "#19a5d8" : "gray", width: "50px", height: "50px", display: "flex", alignItems: "center", justifyContent: "center" }} >
+                {
+                  x
+                }
+              </div>
+            ))
+          }
+
+
+
+        </div>
+
+        <p style={{ textAlign: "center", fontSize: "20px" }}>
+          {
+            message[number - 1]
+          }
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+        <div style={{ display: "flex", alignItems: "center", justifyContent: "space-evenly" }}>
+          {
+            Array.from(["previous", "next"], x => (
+              <button onClick={x == "previous" ? previousBtnHandler : nextBtnHandler} className="btn">{x}</button>
+            ))
+          }
+        </div>
+
+      </div>
+
+    </>
+  )
 }
 
-export default App;
+export default SelectionApp;
